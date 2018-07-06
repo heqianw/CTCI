5506d3583d70021c1a1de7b6cdeca47a14af83f0
He Qian Wang <heqianw@gmail.com>
2018-07-06 18:54:50 -0400

    Created new chapter package and classes

---
+++ test/chapterTwo/testChapterTwo.java
@@ -0,0 +1,14 @@
+package chapterTwo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testChapterTwo {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
