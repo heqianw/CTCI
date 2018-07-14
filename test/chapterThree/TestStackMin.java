1375a6ad9f5738a9a53b07aa9020b8322f3cc0ef
He Qian Wang <heqianw@gmail.com>
2018-07-13 23:11:58 -0400

    Done 3.2 and tests

--- test/chapterThree/TestStackMin.java
+++ test/chapterThree/TestStackMin.java
@@ -8,7 +8,21 @@ class TestStackMin {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		StackMin stack = new StackMin();
+		stack.push(1);
+		stack.push(5);
+		stack.push(2);
+		stack.push(3);
+		stack.push(0);
+		stack.push(7);
+		stack.push(2);
+		
+		assertEquals(0, stack.min());
+		stack.pop();
+		stack.pop();
+		stack.pop();
+		assertEquals(1, stack.min());
+		
 	}
 
 }
