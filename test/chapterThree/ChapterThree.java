f660461c21d0f7c47e8f1d3900f920b85b3b31b9
He Qian Wang <heqianw@gmail.com>
2018-07-13 21:52:24 -0400

    First chapter 3 commit

---
+++ test/chapterThree/ChapterThree.java
@@ -0,0 +1,14 @@
+package chapterThree;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChapterThree {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
