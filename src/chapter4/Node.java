429f10b71100654a1deaf077a7d03f997a747608
He Qian Wang <heqianw@gmail.com>
2018-07-17 18:37:13 -0400

    Changed node class to my style

--- src/chapter4/Node.java
+++ src/chapter4/Node.java
@@ -1,6 +1,20 @@
 package chapter4;
 
 public class Node {
-	public int name;
-	public Node[] children;
+	private int value;
+	private Node[] children;
+	
+	public Node(int value) {
+		this.value = value;
+		this.children = new Node[0];
+	}
+	
+	public void addChildren(Node newChild) {
+		this.children = new Node[this.children.length + 1];
+		this.children[this.children.length - 1] = newChild;
+	}
+	
+	public int getValue() {
+		return this.value;
+	}
 }
